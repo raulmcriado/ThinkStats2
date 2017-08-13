f0af79d59a9c4a90698b4b60b977e15205fd6df6
Raul <r.martinezcriado@gmail.com>
2017-08-13 11:33:02 +0200

    ipdate

--- code/chap02ex.py
+++ code/chap02ex.py
@@ -12,7 +12,12 @@ from operator import itemgetter
 
 import first
 import thinkstats2
+import math
+import numpy as np
 
+import nsfg
+
+import thinkplot
 
 def Mode(hist):
     """Returns the value with the highest frequency.
@@ -21,7 +26,14 @@ def Mode(hist):
 
     returns: value from Hist
     """
-    return 0
+    max_freq = 0
+    for key ,freq in hist.Ttems():
+        if freq> max_freq:
+            max_freq = freq
+            max_key = max_key
+            
+        
+    return max_key
 
 
 def AllModes(hist):
@@ -31,14 +43,56 @@ def AllModes(hist):
 
     returns: iterator of value-freq pairs
     """
-    return []
+    return sorted(hist.Items(),key=lambda item: item1[1],reverse=True)
+    
+def WeightDiff (live,firsts,others):
+    """
+    
+    live: DataFrame of all live births
+    firsts: DataFrame of firsts babies
+    others: DataFrame of others
+    """
+    mean1 = live.totalwgt_lb.mean()
+    mean2 = firsts.totalwgt_lb.mean()
+    mean3 = others.totalwgt_lb.mean()
+    
+    var2 = firsts.totalwgt_lb.var()
+    var3 = others.totalwgt_lb.var()
+    
+    print('Mean')
+    print('First babies', mean2)
+    print('Others', mean3)
 
+    print('Variance')
+    print('First babies', var2)
+    print('Others', var3)
+
+    print('Difference in lbs', mean2 - mean3)
+
+
+    print('Difference relative to mean (%age points)', 
+          (mean2 - mean3) / mean1 * 100)
+
+    d = thinkstats2.CohenEffectSize(firsts.totalwgt_lb, others.totalwgt_lb)
+    print('Cohen d', d)    
+    
+    
+    
 
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
     """
+    
+    preg = nsfg.ReadFemPreg()
+    
+
+    live = preg[preg.outcome == 1]
+    firsts = live[live.birthord == 1]
+    others = live[live.birthord != 1]
+    
+    firsts = live[preg
     live, firsts, others = first.MakeFrames()
     hist = thinkstats2.Hist(live.prglngth)
 
